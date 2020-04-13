53ef737e23a830c8ade616598b6a16011a86c676
Maksym Radko <tttmaximttt@gmail.com>
2020-04-13 15:08:23 +0300

    add buildpack

--- extra/metricbeatRun.sh
+++ extra/metricbeatRun.sh
@@ -1,5 +1,2 @@
-cd metricbeat-7.6.1-linux-x86_64
-cat ./metricbeat.yml
-./metricbeat modules enable system
-./metricbeat setup
-./metricbeat
+echo "Starting metricbeat"
+metricbeat-7.6.1-linux-x86_64/metricbeat -c metricbeat-7.6.1-linux-x86_64/metricbeat.yml &
